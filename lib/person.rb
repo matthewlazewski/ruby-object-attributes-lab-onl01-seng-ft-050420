a1ed2aa589d79df2adeae1bfb1e10c049ceb0cf8
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-11 19:56:59 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name 
+    @name
+  end 
+  
+  def job
+    @job
+  end 
+  
+  def name=(person_name)
+    @name = person_name
+  end 
+  
+  def job=(new_job)
+    @job = new_job
+  end 
+end
