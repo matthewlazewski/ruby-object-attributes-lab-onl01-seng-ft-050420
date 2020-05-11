9936eb405d44510feb38b89cc2861d3be59120a5
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-11 19:55:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,22 @@
 class Dog 
-
-
+  def initialize(name,breed)
+    @name = name
+    @breed = breed
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  def name=(dog_name)
+    @name = dog_name
+  end 
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
 end 
